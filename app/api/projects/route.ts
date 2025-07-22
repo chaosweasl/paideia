18ecead1d20f183c4af1438c8a351261a9ebce97
weasl <iacobserban17danieloff@gmail.com>
2025-07-22 17:06:33 +0300

    h

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
-export async function GET(req) {
+export async function GET(req: Request) {
   const supabase = await createClient();
   const {
     data: { user },
