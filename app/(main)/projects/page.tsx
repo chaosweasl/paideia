7a4263d7a0a249ee1644b0820a5f57a45741d7c1
weasl <iacobserban17danieloff@gmail.com>
2025-07-26 13:46:24 +0300

    responsive cards + fix editor

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -28,7 +28,7 @@ export default function ProjectsPage() {
             <EmptyState />
           </div>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-8">
+          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8 justify-items-center">
             <ProjectList />
           </div>
         )}
